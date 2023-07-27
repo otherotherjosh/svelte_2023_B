350a3411ef2c7a95e1d5ec5f159f23de61554a4d
Josh Finnie <itsotherjosh@gmail.com>
2023-07-28 11:56:30 +1200

    no fucking clue lol

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <h2>HTML practice</h2>
-<li>cat</li>
-<li>dog</li>
-<li>fish</li>
-<li>bird</li>
-<img src="https://svg-files.pixelied.com/60f2e1cc-b0cf-4f69-aaa9-af726478499e/thumb-256px.png"/>
+<ul>
+    <li>cat</li>
+    <li>dog</li>
+    <li>fish</li>
+    <li>bird</li>
+</ul>
+<img width=1000 src="https://svg-files.pixelied.com/60f2e1cc-b0cf-4f69-aaa9-af726478499e/thumb-256px.png"/>
 <p><a href="https://online.op.ac.nz" target="_blank">Otago Polytechnic</a></p>
