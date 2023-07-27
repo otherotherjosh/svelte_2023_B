80c04e46094a096b3aae2bd977014b799589fb21
Josh Finnie <itsotherjosh@gmail.com>
2023-07-28 09:23:07 +1200

    basics of HTML structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>hello what is up </h1>
-<p>waaaah</p>
+<h2>HTML practice</h2>
+<li>cat</li>
+<li>dog</li>
+<li>fish</li>
+<li>bird</li>
+<img src="https://svg-files.pixelied.com/60f2e1cc-b0cf-4f69-aaa9-af726478499e/thumb-256px.png"/>
+<p><a href="https://online.op.ac.nz">Otago Polytechnic</a></p>
