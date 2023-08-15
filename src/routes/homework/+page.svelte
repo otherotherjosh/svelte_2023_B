f1236566932b9f1388c8eaf8a80503311bbace65
Josh Finnie <itsotherjosh@gmail.com>
2023-08-16 09:09:59 +1200

    homework

---
+++ src/routes/homework/+page.svelte
@@ -0,0 +1,32 @@
+<svelte:head>
+    <link rel="stylesheet" href="css/homework.css"/>
+</svelte:head>
+
+<div class="main" style="margin-bottom: 25px">
+    <h1>homework dumping ground</h1>
+</div>
+
+<style>
+    
+    .homework h1{ background: lightblue; }
+    p{ background: lightgreen; }
+    ul{ background: lightsalmon; }
+    .footer{ background: lightslategray; }
+    
+</style>
+
+<div class="homework">
+    <h1>Main heading</h1>
+    <p>
+    	This is some informative content.
+    </p>
+    <ul>
+      <li>One</li>
+      <li>Two</li>
+      <li>Three</li>
+      <li>Four</li>
+    </ul>
+    <div class="footer">
+    	Footer content
+    </div>
+</div>
