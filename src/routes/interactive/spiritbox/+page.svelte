97bab0489ddfd5261acccc07c2e20d8745b676a7
Josh Finnie <itsotherjosh@gmail.com>
2023-09-15 11:51:54 +1200

    spiritbox prototype

---
+++ src/routes/interactive/spiritbox/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	let m = { x: 0, y: 0 };
+
+	function handleMousemove(event) {
+		m.x = event.clientX;
+		m.y = event.clientY;
+	}
+
+	let spiritboxLocation = () => {
+		console.log("i am behind you >:3");
+	}
+
+	let spiritboxAge = () => {
+		console.log("i am very old and my bones hurt");
+	}
+
+	let spiritboxFriendliness = () => {
+		console.log("i will hunt you lmao")
+	}
+</script>
+
+<button on:click={spiritboxLocation}>Where are you?</button>
+<button on:click={spiritboxAge}>How old are you?</button>
+<button on:click={spiritboxFriendliness}>Are you friendly?</button>
