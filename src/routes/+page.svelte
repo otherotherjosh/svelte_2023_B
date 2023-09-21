a32972af60c8fe5d924d2eee51a7bc6e8ef5219d
Josh Finnie <itsotherjosh@gmail.com>
2023-09-22 11:16:22 +1200

    added cats for some reason

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+    const BASE_URL = `https://api.unsplash.com`;
+    const CLIENT_ID = `z40TIIyaq-YDho1wKR7UKenm1vj6ZA6nio7GVDJ-dLE`;
+
+    let cats = [];
+
+    fetch(`${BASE_URL}/search/photos?query=cats&client_id=${CLIENT_ID}`)
+    .then(res => res.json())
+    .then(data => {
+        cats = data.results
+    })
+</script>
+
+<style>
+    .gallery{
+        width:100%;
+        display: grid;
+        grid-template: repeat(1fr, 3) / repeat(100px, 3);
+    }
+
+    .gallery div{
+        /* make the cats visible somehow */
+    }
+</style>
+
+<div class="gallery">
+    {#each cats as cat}
+        <div style="background-image:url({cat.urls.regular})"></div>
+    {/each}
+</div>
+
