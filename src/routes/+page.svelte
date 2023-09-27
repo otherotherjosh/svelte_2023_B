1f02e0fd4702f3021a053cb52c221ca7aa001874
otherotherjosh <itsotherjosh@gmail.com>
2023-09-27 18:57:58 +1300

    imgBackground url now expected in HTML

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<div class=imgBackground></div>
+<div class=imgBackground style="background-image:url(https://oyster.ignimgs.com/mediawiki/apis.ign.com/phasmophobia/e/e2/Willow_Street_House_2.JPG);
+"></div>
 <h1>meow.</h1>
