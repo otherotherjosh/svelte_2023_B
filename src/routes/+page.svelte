f3c28667f8b8f9471c43b07a110940f072808737
Josh <itsotherjosh@gmail.com>
2023-09-29 20:41:08 +1300

    renamed film grain element and reduced opacity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 
 <div class="hero">
-    <div class="noise"></div>
+    <div class="filmGrain"></div>
     <div class="imgBackground">
         <video autoplay muted loop>
             <source src="/img/rain cat demo 1.mp4" type="video/mp4">
