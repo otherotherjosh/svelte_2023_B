07cb6f1c7c73ade3739031b993f5c130fc4f9ad4
Josh <itsotherjosh@gmail.com>
2023-10-12 19:40:32 +1300

    louder noise and improved text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,10 @@
         "there have been pawprints spotted on the ceiling",
         "one of these rooms has yarn all over the floor",
         "mysterious scratch marks cover the wallpapers",
-        "don't let it fall asleep on you or you'll never get up",
-        "you hear a faint purring from around the corner",
+        "don't let it fall asleep on you ~ or you'll never get up",
+        "you hear a faint purring from behind the windows",
         "those items weren't supposed to be on the floor",
-        "you feel like you are being watched (from under the furniture)",
+        "it feels like you are being watched (from under the furniture)",
         "meow"
     ];
     let heroText = heroTextOptions[Math.floor(Math.random()*heroTextOptions.length)] + "...";
