aa301d6bd4f42510c4b2ff37f9ba18a1985d15e7
otherotherjosh <itsotherjosh@gmail.com>
2023-10-27 11:09:40 +1300

    fetched image for each cat breed

--- src/routes/guide/+page.svelte
+++ src/routes/guide/+page.svelte
@@ -5,10 +5,10 @@
     const BASE_URL = "https://api.thecatapi.com/v1";
     let cats = [];
     let breeds = [
-        {name: "java", evidence: [0, 3, 4]},
-        {name: "mcoo", evidence: [1, 4, 5]},
-        {name: "ycho", evidence: [0, 1, 6]}
-    ]
+        {id: "java", evidence: [0, 3, 4], img: ""},
+        {id: "mcoo", evidence: [1, 4, 5], img: ""},
+        {id: "ycho", evidence: [0, 1, 6], img: ""}
+    ];
     const evidence = [
         "D.O.T.S Projector",
         "Ghost Writing",
@@ -20,19 +20,24 @@
     ];
 
     onMount( async () => {
-        breeds.foreach()
+        breeds.forEach(breed => addBreedData(breed))
     });
 
-    async function addBreed(breed) {
-        let resp = await fetch(`${BASE_URL}/images/search?breed_ids={breed.id}?limit=1&api_key=${key}`);
+    async function addBreedData(breed) {
+        let resp = await fetch(`${BASE_URL}/images/search?breed_ids=${breed.id}&api_key=${key}`);
         let data = await resp.json();
+        breed.img = data[0].url;
+        console.log(breed);
+        cats.push(breed);
+        cats = cats;
     }
 </script>
 
 <section class="gallery">
     {#each cats as cat}
         <div>
-            <div class="imgBackground" style=background-image:url({cat.url})></div>
+            <div class="imgBackground" style=background-image:url({cat.img})></div>
+            <h2>{cat.id}</h2>
         </div>
     {/each}
 </section>
