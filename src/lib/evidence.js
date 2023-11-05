8dc366c09da430e36d125f296f24f3f44bbbf53b
Josh <itsotherjosh@gmail.com>
2023-11-05 20:17:34 +1300

    added evidence pictures

--- src/lib/evidence.js
+++ src/lib/evidence.js
@@ -3,38 +3,41 @@ export const evidence = [
         name: 'D.O.T.S Projector',
         description: 
         "This cat may be revealed temporarily when shining the Dots On The Surface laser grid somewhere in the room. " +
-        "Be careful not to shine these lasers too close to yourself as this increases your chances of getting lunged at!",
-        image: "../img/placeholder.jpg"
+        "Warning: cats have been reported lunging at the laser.",
+        image: "../img/evidence_dots.png"
     },
     {	name: 'Cat Writing',
         description:
         "They may not be able to read, but if you leave an open book and a pen on the ground somewhere near this cat, " +
         "you will be surprised at the legibility of the squiggles you find.",
-        image: "../img/placeholder.jpg"
+        image: "../img/evidence_writing.png"
     },
     {	name: 'EMF Level 5',
         description:
         "Electro Magnetic Fluffiness Level 5",
-        image: "../img/placeholder.jpg"
+        image: "../img/evidence_emf.png"
     },
     {	name: 'Cat Orb',
         description:
-        "Orbs found through infrared night vision cameras",
+        "It may be dead skin... it could be fur... it might even be dust off a scratched up wall. " +
+        "Using an infrared night vision camera, detectives have found floating \"orbs\" around this cat.",
         image: "../img/evidence_orb.png"
     },
     {	name: 'Ultraviolet',
         description:
-        "UV light revealing paw prints",
+        "One thing all cats have in common is they all love pushing doors open, running up walls and walking on the ceiling. " +
+        "We're not sure why, but not all cats leave pawprints when they touch those surfaces. " +
+        "These cats, however, will leave paw residue which can be seen when under a UV light.",
         image: "../img/evidence_ultraviolet.png"
     },
     {	name: 'Freezing Temperatures',
         description:
         "it gets cold",
-        image: "../img/placeholder.jpg"
+        image: "../img/evidence_freezing.png"
     },
     {	name: 'Litter Box',
         description:
         "hearing meows from the litter box",
-        image: "../img/placeholder.jpg"
+        image: "../img/evidence_litterbox.png"
     }
 ];
