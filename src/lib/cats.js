f1b6cda83cfd6a45413db6185362f9ea60ef516b
otherotherjosh <itsotherjosh@gmail.com>
2023-11-07 10:34:53 +1300

    finished creating cats array

--- src/lib/cats.js
+++ src/lib/cats.js
@@ -2,12 +2,25 @@ export let cats = [
     { index: 35, evidence: [0, 3, 4] }, // Banshee - java
     { index: 40, evidence: [1, 4, 5] }, // Demon - mcoo
     { index: 66, evidence: [0, 1, 6] }, // Deogen - ycho
-    { index: 45, evidence: [3, 4, 5] }, // Hantu - norw
+    { index: 51, evidence: [0, 2, 4] }, // Goryo - ragd
+    { index: 59, evidence: [3, 4, 5] }, // Hantu - snow
+    { index: 43, evidence: [2, 4, 5] }, // Jinn - munc
+    { index: 12, evidence: [1, 3, 6] }, // Mare - bomb
+    { index: 33, evidence: [1, 5, 6] }, // Moroi - hima
+    { index: 48, evidence: [1, 2, 4] }, // Myling - pers
+    { index: 45, evidence: [2, 3, 4] }, // Obake - norw
     { index: 61, evidence: [0, 2, 5] }, // Oni - sphy
+    { index: 47, evidence: [3, 5, 6] }, // Onryo - orie
+    { index: 49, evidence: [0, 4, 6] }, // Phantom - pixi
+    { index: 10, evidence: [1, 4, 6] }, // Poltergeist - beng
+    { index: 25, evidence: [0, 2, 3] }, // Raiju - cypr
     { index: 29, evidence: [1, 3, 5] }, // Revenant - emau
     { index: 0, evidence: [1, 2, 5] }, // Shade - abys
     { index: 4, evidence: [1, 2, 6] }, // Spirit - asho
+    { index: 27, evidence: [0, 1, 3] }, // Thaye - dons
     { index: 15, evidence: [4, 5, 6] }, // The Mimic - bure
     { index: 56, evidence: [2, 5, 6] }, // The Twins - siam
-    { index: 54, evidence: [0, 3, 6] } // Yokai - sfol
+    { index: 32, evidence: [0, 2, 6] }, // Wraith - hbro
+    { index: 54, evidence: [0, 3, 6] }, // Yokai - sfol
+    { index: 38, evidence: [0, 3, 5] } // Yurei - kuri
 ];
